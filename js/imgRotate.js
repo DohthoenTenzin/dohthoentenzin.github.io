35d572d765724c53bab77dc7e883b86cf8981de0
Tenzin Dohthoen <56147811+DohthoenTenzin@users.noreply.github.com>
2020-10-05 10:45:34 -0600

    Add files via upload

--- js/imgRotate.js
+++ js/imgRotate.js
@@ -1,17 +1,45 @@
-var header = $('#porn-tile');
+var porn = $('#porn-tile');
+var about = $('#about-tile');
+var camshow = $('#camshow-tile');
 
-var backgrounds = new Array(
-    'url("https://cl.phncdn.com/gif/12254441.gif")'
-  , 'url("../images/porn.webp")'
+// array containing gifs for the porn tile
+var pornGifs = new Array(
+  'url("https://cl.phncdn.com/gif/12254441.gif")',
+  'url("https://i.gyazo.com/6d7298df5d0719e24c58e0bc83f9e133.gif")'
 );
 
+// array containing gifs for the about tile
+var aboutGifs = new Array(
+  'url("../images/tenzin.jpg")',
+  'url("../images/tenzinflash.png")'
+)
+
+// array containing gifs for the camshow tile
+var camshowGifs = new Array(
+  'url("../images/camshows.webp")',
+  'url("../images/camshow1.webp")',
+  'url("../images/camshow2.webp")',
+  'url("../images/camshow3.webp")'
+)
+
 var current = 0;
+var camshowCurrent = 0;
 
+// function to change the images
 function nextBackground() {
-    current++;
-    current = current % backgrounds.length;
-    header.css('background-image', backgrounds[current]);
+  current++;
+  camshowCurrent++;
+  current = current % pornGifs.length;
+  current = current % aboutGifs.length;
+  camshowCurrent = camshowCurrent % camshowGifs.length;
+  porn.css('background-image', pornGifs[current]);
+  about.css('background-image', aboutGifs[current]);
+  camshow.css('background-image', camshowGifs[camshowCurrent]);
 }
-setInterval(nextBackground, 6000);
 
-header.css('background-image', backgrounds[0]);
+// how often to call the function that changes the images
+setInterval(nextBackground, 7000);
+
+porn.css('background-image', pornGifs[0]);
+about.css('background-image', aboutGifs[0]);
+camshow.css('background-image', camshowGifs[0]);
